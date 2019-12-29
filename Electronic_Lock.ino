de0be94bc5abaf3f1d763ce2177ad7acf8739922
jetaehyun <jetaehyun0@gmail.com>
2019-12-29 15:58:33 -0800

    Added code for password system

--- Electronic_Lock.ino
+++ Electronic_Lock.ino
@@ -1,38 +1,80 @@
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 
-const byte ROWS = 4; 
-const byte COLS = 4; 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-char hexaKeys[ROWS][COLS] = {
-  {'A', '3', '2', '1'},
-  {'B', '6', '5', '4'},
-  {'C', '9', '8', '7'},
-  {'D', '#', '0', '*'}
+enum state {
+  savePower,
+  check,
+  newPassword
 };
 
-byte rowPins[ROWS] = {9, 8, 7, 6}; 
-byte colPins[COLS] = {5, 4, 3, 2}; 
+const byte ROWS = 4;
+const byte COLS = 4;
+LiquidCrystal lcd(A4, A5, A0, A1, A2, A3);
+volatile state s = savePower;
 
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
 
-void setup(){
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
+
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+int ogPassword[4] = {1, 2, 3, 4};
+
+void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  lcd.begin(16, 2);
-  lcd.setCursor(0,1);
-  lcd.print("Lock Activated");
+  lcd.begin(16, 0);
+  lcd.setCursor(0, 1);
+  lcd.print("Lock Activated...");
+  systemDelay(1000);
+  lcd.clear();
 }
-  
-void loop(){
-  char customKey = customKeypad.getKey();
-  digitalWrite(13, LOW);
-  if (customKey){
-    Serial.println(customKey);
-    digitalWrite(13, HIGH);
-    delay(3000);
-  }
 
-  
+void loop() {
+  char customKey = customKeypad.getKey();
+  if (customKey) {
+    Serial.println(customKey);
+    lcd.print(customKey);
+    systemDelay(1000);
+    lcd.clear();
+  }
+}
+
+bool checkPassword(int* userPassword) {
+  bool isCorrect = false;
+  lcd.clear();
+  if (isCorrect) {
+    lcd.print("The password was correct...");
+    systemDelay(1000);
+    lcd.clear();
+    return true;
+  }
+  lcd.print("Incorrect password...");
+  systemDelay(1000);
+  lcd.clear();
+  return false;
+}
+void printIt() { // For debugging purposes....
+  for (int i = 0; i < 4; i++) {
+    lcd.print(*(ogPassword + i));
+  }
+}
+
+void reset(int* password) {
+  for (int i = 0; i < 4; i++) {
+    *(password + i) = 0;
+  }
+}
+
+void systemDelay(int wait) {
+  long unsigned int timeStamp = millis();
+
+  while ((millis() - timeStamp) < wait) {
+    // do nothing
+  }
 }
